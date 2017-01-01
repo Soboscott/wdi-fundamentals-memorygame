042f850c11d5951d7e88cb7ec5df9383117344f2
Scott Landry <scott.landry01772@yahoo.com>
2017-01-01 12:32:21 -0500

    added loop to cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,4 +2,12 @@ console.log("JS file is connected to HTML! Woo!")
 var CardOne = "Queen";
 var Cardtwo = "King";
 var CardThree = "Queen";
-Var CardFour = "King";
+var CardFour = "King";
+if (CardOne) === (CardThree) {
+	window.alert(" You found a match!")
+
+}
+else if (Cardtwo === CardFour) {
+	window.alert("you found a match!")
+}
+else {window.alert("Sorry, try again.")}
